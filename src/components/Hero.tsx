57621dbade8950b2a5968e7cce29b3444a436622
arshalabbas <arshalabbas@gmail.com>
2024-09-19 01:15:42 +0530

    Initialised and created the layout

---
+++ src/components/Hero.tsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
+
+const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth > 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => window.removeEventListener("resize", handleVideoSrcSet);
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title" id="hero">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p>From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
